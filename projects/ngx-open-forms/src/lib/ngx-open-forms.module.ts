42393df0e4e1dc7c13f198d05fdbd9fdad3e0513
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2021-10-07 17:05:39 -0300

    exports error message pipe

--- projects/ngx-open-forms/src/lib/ngx-open-forms.module.ts
+++ projects/ngx-open-forms/src/lib/ngx-open-forms.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ErrorMessagePipe } from './error-message.pipe';
 @NgModule({
-    declarations: [ErrorMessagePipe]
+    declarations: [ErrorMessagePipe],
+    exports: [ErrorMessagePipe]
 })
 export class OpenFormsModule { }
