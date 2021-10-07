521a92e541282cae9547e9bea4769a715aa74208
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2021-10-07 18:47:51 -0300

    trying solution for exporting error message locales

--- projects/ngx-open-forms/src/locales/error-messages.pt.ts
+++ projects/ngx-open-forms/src/locales/error-messages.pt.ts
@@ -1,11 +1,9 @@
-import { ErrorMessages } from "./error-message-fn";
-
-export const PT_ERROR_MESSAGES: ErrorMessages = {
-    email: _ => 'email inválido',
-    max: data => `deve ser menor que ${data.max}`,
-    maxLength: data => `máximo ${data.requiredLength} caracteres`,
-    min: data => `deve ser maior que ${data.min}`,
-    minLength: data => `mínimo ${data.requiredLength} caracteres`,
-    pattern: _ => 'valor inválido',
-    required: _ => 'obrigatório',
+export const PT_ERROR_MESSAGES = {
+    email: () => 'email inválido',
+    max: (data: any) => `deve ser menor que ${data.max}`,
+    maxLength: (data: any) => `máximo ${data.requiredLength} caracteres`,
+    min: (data: any) => `deve ser maior que ${data.min}`,
+    minLength: (data: any) => `mínimo ${data.requiredLength} caracteres`,
+    pattern: () => 'valor inválido',
+    required: () => 'obrigatório',
 };
