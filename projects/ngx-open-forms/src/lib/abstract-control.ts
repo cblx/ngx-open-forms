17fecfaf8895df4097b136fac15b2f76b8d32ca3
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2021-10-07 16:58:57 -0300

    fixes abstract control

--- projects/ngx-open-forms/src/lib/abstract-control.ts
+++ projects/ngx-open-forms/src/lib/abstract-control.ts
@@ -2,5 +2,5 @@ import { AbstractControl } from "@angular/forms";
 import { SchemaLike } from "./schema-like";
 
 export interface OpenAbstractControl extends AbstractControl {
-    get schema(): SchemaLike;
+    readonly schema: SchemaLike;
 }
