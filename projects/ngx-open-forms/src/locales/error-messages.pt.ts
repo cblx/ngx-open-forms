dc2bacfcf9551d7d631566fe5fccad40cd2f662d
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2021-10-08 11:14:47 -0300

    fix minlength and maxlength messages

--- projects/ngx-open-forms/src/locales/error-messages.pt.ts
+++ projects/ngx-open-forms/src/locales/error-messages.pt.ts
@@ -1,9 +1,11 @@
 export const PT_ERROR_MESSAGES = {
     email: 'email inválido',
     max: (data: any) => `deve ser menor que ${data.max}`,
-    maxLength: (data: any) => `máximo ${data.requiredLength} caracteres`,
+    maxlength: (data: any) => `máximo ${data.requiredLength} caracteres`,
     min: (data: any) => `deve ser maior que ${data.min}`,
-    minLength: (data: any) => `mínimo ${data.requiredLength} caracteres`,
+    minlength: (data: any) => 
+        !data.actualLength ? `mínimo ${data.requiredLength} caracteres`
+        : `utilize mais ${data.requiredLength - data.actualLength} caracteres`,
     pattern: 'valor inválido',
     required: 'obrigatório',
 };
