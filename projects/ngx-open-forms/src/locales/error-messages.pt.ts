2f10969a3e3d8fdb2dcdca09f1ba35a7e4588973
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2021-10-08 11:37:12 -0300

    adjust min/max messages

--- projects/ngx-open-forms/src/locales/error-messages.pt.ts
+++ projects/ngx-open-forms/src/locales/error-messages.pt.ts
@@ -1,8 +1,8 @@
 export const PT_ERROR_MESSAGES = {
     email: 'email inválido',
-    max: (data: any) => `deve ser menor que ${data.max}`,
+    max: (data: any) => `máximo deve ser ${data.max}`,
     maxlength: (data: any) => `máximo ${data.requiredLength} caracteres`,
-    min: (data: any) => `deve ser maior que ${data.min}`,
+    min: (data: any) => `mínimo deve ser ${data.min}`,
     minlength: (data: any) => 
         !data.actualLength ? `mínimo ${data.requiredLength} caracteres`
         : `utilize mais ${data.requiredLength - data.actualLength} caracteres`,
